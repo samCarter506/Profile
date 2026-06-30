4e1cdb54d08a8eee5317a7d3081fa26f1cf304d2
thamaesamuel <thamaesamuel@gmail.com>
2026-06-30 17:37:03 +0200

    Image added

--- src/Pages/Hero.js
+++ src/Pages/Hero.js
@@ -1,20 +1,31 @@
-import './Hero.css'
+import "./Hero.css";
 import { portfolioData } from "./portfolioData";
 
 export default function Hero() {
   return (
     <section className="hero">
+      <div className="hero-content">
+        <div className="hero-text">
+          <h1>
+            Hi, I'm <span>{portfolioData.name}</span>
+          </h1>
 
-      <h1>
-        Hi, I'm <span>{portfolioData.name}</span>
-      </h1>
+          <h2>{portfolioData.role}</h2>
 
-      <h2>{portfolioData.role}</h2>
+          <p>{portfolioData.intro}</p>
 
-      <p>{portfolioData.intro}</p>
+          <a href="/Samuel_thamae.docx" download>
+            <button>Download CV</button>
+          </a>
+        </div>
 
-      <button>Download CV</button>
-
+        <div className="hero-image">
+          <img
+            src="/Samuelt.png"
+            alt="Samuel Thamae"
+          />
+        </div>
+      </div>
     </section>
   );
 }
